41c292ba85033002c3320143eb8087bc2b5878de
HoodRusher <boutlpyt@gmail.com>
2022-09-09 01:38:40 +0200

    Delete sd_infoitems directory

--- sd_infoitems/config.lua
+++
@@ -1,7 +0,0 @@
-Cfg = Cfg or {}
-
-Cfg.Framework = 'qbCore' -- ESX / qbCore
-
-Cfg.Items = {
-  { itemName = "stgb", itemLabel = "Strafgesetzbuch", fileName = "stgb", maxAmount = "3", itemWeight = "" } -- Filename = Name of the Document which must be in "sd_infoitems/ui/data/"
-}
